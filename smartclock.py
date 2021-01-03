1650353483949bd1d980fdd3ad807c498039868d
Axel1556 <kevin.rph.lee@gmail.com>
2021-01-02 19:33:22 -0800

    added gitignore

--- smartclock.py
+++ smartclock.py
@@ -1,21 +1,43 @@
 from time import strftime
 from tkinter import *
+from datetime import date
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
+
+api_key = "9ec1418c0976ea68fcf8ef36b99f59f4"
 
 
 window = Tk()
 window.title("")
-window.geometry("200x80")
 window.configure(bg="green")
 window.resizable(False, False)
 
-clock_label = Label(window, bg="green", fg="white", font = ("Times", 30, 'bold'), relief='flat')
-clock_label.place(x = 20, y = 20)
+today = date.today()
 
-def update_label():
+
+clock_label = Label(window, bg="green", fg="white", font = ("Times", 30, 'bold'), relief='flat')
+clock_label.grid(column = 0, row= 0, sticky = 'w')
+
+date_label = Label(window, bg="green", fg="white", font = ("Times", 30, 'bold'), relief='flat')
+date_label.grid(column = 0, row= 1, sticky = 'w')
+
+def update_time():
     current_time = strftime('%H: %M: %S')
     clock_label.configure(text = current_time)
-    clock_label.after(80, update_label)
+    clock_label.after(80, update_time)
 
-update_label()
+# def update_date():
+#     current_date = today.strftime("%d-%b-%Y")
+#     date_label.configure(text = current_date)
+#     date_label.after(80, update_date)
+
+print('Today ' + today.strftime("%d-%b-%Y"))
+
+# print(os.getenv('foo'))
+
+
+update_time()
+# update_date()
 window.mainloop()
